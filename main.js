c1d0c3ab033b41d16ed4b4a16191c66287c3bc57
mdsajalali <mdsajalali2020@gmail.com>
2023-10-15 11:02:42 +0600

    api added hero section

---
+++ main.js
@@ -0,0 +1,41 @@
+const heroItem = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/category/1000"
+  );
+  const data = await res.json();
+  const contents = data.data;
+  displayContent(contents);
+};
+heroItem();
+
+const displayContent = (contents) => {
+  const mainContainer = document.getElementById("main-content");
+
+  contents.forEach((content) => {
+    const contentCard = document.createElement("div");
+    contentCard.classList = `p-2 bg-gray-100 shadow-xl rounded `;
+    contentCard.innerHTML = `
+    <figure>
+              <img class="w-full h-[200px] "
+                src="${content.thumbnail}"
+                alt=""
+              />
+              </figure>
+    
+              <div  class="mt-3 flex gap-3">
+                <img class="w-10 h-10 rounded-full "
+                src="${content.authors[0].profile_picture}"
+                alt=""
+              />
+              
+              <div class="">
+              <p class="">${content.authors[0].profile_name}</p>
+              <h1>${content.others.views}</h1>
+              </div>
+            
+              </div>
+    `;
+
+    mainContainer.append(contentCard);
+  });
+};
